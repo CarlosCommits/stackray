5ac28f0e08449d705e40ca72368381ddb2a647f7
Carlos <41172581+CarlosCommits@users.noreply.github.com>
2026-03-26 16:14:52 -0400

    fix: removed @aliases in worker to fix runtime crash

--- lib/server/storage/screenshots.ts
+++ lib/server/storage/screenshots.ts
@@ -4,7 +4,7 @@ import { stat } from "node:fs/promises";
 import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-import { env } from "@/lib/env/server";
+import { env } from "../../env/server.ts";
 
 function getBucketConfig() {
   if (
