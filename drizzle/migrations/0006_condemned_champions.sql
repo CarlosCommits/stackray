e75572bca52e351a96279d35245b369689b76b77
Carlos <41172581+CarlosCommits@users.noreply.github.com>
2026-04-10 11:08:09 -0400

    chore: remove late legacy drizzle migrations

--- drizzle/migrations/0006_condemned_champions.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "scan_result_nuclei_matches" ADD COLUMN "subject" text;--> statement-breakpoint
-ALTER TABLE "scan_result_nuclei_matches" ADD COLUMN "subject_type" text;--> statement-breakpoint
-ALTER TABLE "scan_result_nuclei_runs" ADD COLUMN "domain_target" text;
