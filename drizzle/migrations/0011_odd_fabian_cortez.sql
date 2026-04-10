4624bf9fb935757c923759e62bc7d2143722da6a
Carlos <carloscanas942@gmail.com>
2026-04-10 11:08:09 -0400

    chore: remove final legacy drizzle migrations

--- drizzle/migrations/0011_odd_fabian_cortez.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "instance_settings" ADD COLUMN "custom_domain_hostname" text;--> statement-breakpoint
-ALTER TABLE "instance_settings" ADD COLUMN "custom_domain_dns_verified_at" timestamp with time zone;--> statement-breakpoint
-ALTER TABLE "instance_settings" ADD COLUMN "custom_domain_app_verified_at" timestamp with time zone;--> statement-breakpoint
-ALTER TABLE "instance_settings" ADD COLUMN "custom_domain_last_checked_at" timestamp with time zone;
