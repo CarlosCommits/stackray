c837142a066d5da9b0d814789059c46919a69a3c
Carlos <carloscanas942@gmail.com>
2026-04-10 11:08:09 -0400

    chore: remove late legacy drizzle migrations

--- drizzle/migrations/0007_little_toro.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "scan_result_nuclei_runs" ADD COLUMN "original_domain_target" text;--> statement-breakpoint
-ALTER TABLE "scan_result_nuclei_runs" ADD COLUMN "final_domain_target" text;
--- drizzle/migrations/0008_aberrant_stackray_tokens.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "api_token_access_enabled" boolean DEFAULT true NOT NULL;--> statement-breakpoint
-ALTER TABLE "api_tokens" ADD COLUMN "token_hint" text;
