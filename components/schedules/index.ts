bfebdf9015b6e7601cb7eb8c892be5bffa448dde
Carlos <41172581+CarlosCommits@users.noreply.github.com>
2026-05-11 22:42:20 -0400

    fix(schedules): polish schedule UI exports

--- components/schedules/index.ts
+++
@@ -1,4 +0,0 @@
-export { CreateScheduleDialog } from "./create-schedule-dialog"
-export type { CreateScheduleSeed } from "./create-schedule-dialog"
-export { ScheduleList } from "./schedule-list"
-export { SchedulesClient } from "./schedules-client"
