a75230dcac67114d39aab01f126f5d6c4287054e
Carlos <41172581+CarlosCommits@users.noreply.github.com>
2026-06-10 21:25:42 -0400

    fix(dev): add allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  allowedDevOrigins: ["192.168.1.203", "mini", "100.90.230.40"],
 };
 
 export default nextConfig;
